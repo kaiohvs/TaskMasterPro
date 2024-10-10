248d65036daddc6cc6af94991603cab15866f03b
Kaio Vasconcelos <Kaiohvs@gmail.com>
2024-10-10 16:40:27 -0300

    Ajuste na tela de login e register

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,7 +108,7 @@ namespace TaskMasterPro.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("/Identity/Account/Login");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
